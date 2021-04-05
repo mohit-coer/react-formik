3813eaa92f4e0604b09da0913ce2edc2b982825f
mohit-sofka <mohit.kumar@sofka.com.co>
2021-04-05 20:14:43 +0530

    SEO Added in Project

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import { fetchUsers } from "./Redux";
 import React, { Component, useEffect } from "react";
 import { connect } from "react-redux";
 import Forms from "./forms";
-
+import { Link, Route, Switch } from "react-router-dom";
+import Home from "./Home";
 function App(props) {
   return (
     <>
-      <Forms />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/forms">Forms</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/forms">
+          <Forms />
+        </Route>
+      </Switch>
     </>
   );
 }
